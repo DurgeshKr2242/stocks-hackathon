9733c456c5ea1dc7cd45d7a8452d2545f9168e81
DurgeshKr2242 <code.durgesh@gmail.com>
2022-01-22 23:52:42 +0530

    integration done

--- components/MainPage/MainPage.js
+++ components/MainPage/MainPage.js
@@ -4,20 +4,32 @@ import { FcNegativeDynamic } from "react-icons/fc";
 import { FcNeutralTrading } from "react-icons/fc";
 import axios from "axios";
 import News from "./News";
+
 const MainPage = () => {
   const [sentimentPositive, setSentimentPositive] = useState(0);
   const [sentimentNeutral, setSentimentNeutral] = useState(0);
   const [sentimentNegative, setSentimentNegative] = useState(0);
   const [searchTerm, setSearchTerm] = useState("");
+  const [allNews, setAllNews] = useState([]);
+  const [companyQuote, setCompanyQuote] = useState(null);
+
+  const quoteUrl =
+    "https://financialmodelingprep.com/api/v3/quote/AAPL?apikey=75801e80587754d65c6b5eae45da6f34";
 
   const getSentiment = async () => {
     const res = await axios.get(`http://127.0.0.1:5000/${searchTerm}`);
-    console.log(res.data.Positive);
-    console.log(res.data.Neutral);
-    console.log(res.data.Negative);
+    const newsRes = await axios.get(`http://192.168.29.156:8000/${searchTerm}`);
+    const quoteRes = await axios.get(
+      `https://financialmodelingprep.com/api/v3/quote/${searchTerm.toLocaleUpperCase()}?apikey=75801e80587754d65c6b5eae45da6f34`
+    );
+
+    console.log(quoteRes);
+    setCompanyQuote(quoteRes.data[0]);
+    setAllNews(newsRes.data);
     setSentimentPositive(res.data.Positive);
     setSentimentNeutral(res.data.Neutral);
     setSentimentNegative(res.data.Negative);
+
     //   setSentiment(res.data.Positive);
     //   console.log(sentiment.Positive);
   };
@@ -44,33 +56,121 @@ const MainPage = () => {
             SHOWWWW
      */}
 
-      <div className="flex flex-col items-center w-full gap-12">
-        <div className="flex gap-6 mobileL:flex-row flex-col">
-          <p className="bg-bgBlackSec border-[1px] flex items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
-            <p className="text-white font-bold">
-              {(sentimentPositive * 100).toPrecision(2)}
-            </p>
-            Positive
-            <FcPositiveDynamic />
+      <div className="flex flex-col items-center w-full gap-20 pt-10">
+        <div className="flex flex-col items-start gap-6 w-full px-20">
+          <p className="bg-green-700 tracking-wide text-sm text-white font-bold px-3 rounded-md py-1">
+            Predicted Sentiment :
+          </p>
+          <div className="flex gap-6 mobileL:flex-row flex-col w-full justify-center">
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              <p className="text-white font-bold">
+                {(sentimentPositive * 100).toPrecision(2)}
+              </p>
+              Positive
+              <FcPositiveDynamic />
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              <p className="text-white font-bold">
+                {(sentimentNeutral * 100).toPrecision(2)}
+              </p>
+              Neutral
+              <FcNeutralTrading />
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              <p className="text-white font-bold">
+                {(sentimentNegative * 100).toPrecision(2)}
+              </p>
+              Negative
+              <FcNegativeDynamic />
+            </div>
+          </div>
+        </div>
+
+        {/* ``````````````````````` */}
+
+        <div className="flex flex-col items-start gap-6 w-full px-20">
+          <p className="bg-green-700 tracking-wide text-sm text-white font-bold px-3 rounded-md py-1">
+            Companies Info :
           </p>
 
-          <p className="bg-bgBlackSec border-[1px] flex-nowrap flex items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
-            <p className="text-white font-bold">
-              {(sentimentNeutral * 100).toPrecision(2)}
-            </p>
-            Neutral
-            <FcNeutralTrading />
-          </p>
-          <p className="bg-bgBlackSec border-[1px] flex items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
-            <p className="text-white font-bold">
-              {(sentimentNegative * 100).toPrecision(2)}
-            </p>
-            Negative
-            <FcNegativeDynamic />
-          </p>
+          <div className="flex gap-6 flex-wrap mobileL:flex-row flex-col w-full justify-center">
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              Symbol :
+              <p className="text-white font-bold">{companyQuote.symbol}</p>
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              Name :<p className="text-white font-bold">{companyQuote.name}</p>
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              Price :
+              <p className="text-white font-bold">{companyQuote.price}</p>
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              % change :
+              <p className="text-white font-bold">
+                {companyQuote.changesPercentage}
+              </p>
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              Change :
+              <p className="text-white font-bold">{companyQuote.change}</p>
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              Day Low :
+              <p className="text-white font-bold">{companyQuote.dayLow}</p>
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              Day High :
+              <p className="text-white font-bold">{companyQuote.dayHigh}</p>
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              Year Low :
+              <p className="text-white font-bold">{companyQuote.yearLow}</p>
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              Year High :
+              <p className="text-white font-bold">{companyQuote.yearHigh}</p>
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              Market Cap :
+              <p className="text-white font-bold">{companyQuote.marketCap}</p>
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              Volume :
+              <p className="text-white font-bold">{companyQuote.volume}</p>
+            </div>
+
+            <div className="bg-bgBlackSec border-[1px] flex flex-nowrap items-center gap-2 border-gray-300/30 py-1 px-4 rounded-md text-bgWhiteSec/80">
+              Exchange :
+              <p className="text-white font-bold">{companyQuote.exchange}</p>
+            </div>
+          </div>
         </div>
-        <div className="flex gap-10 flex-wrap">
-          <News />
+
+        {/* ````````````````````````````` */}
+
+        <div className="flex flex-col items-start gap-6 w-full px-10">
+          <p className="bg-green-700 tracking-wide text-sm text-white font-bold px-3 rounded-md py-1 ml-10">
+            Company News :
+          </p>
+
+          <div className="flex gap-4 flex-wrap justify-center mb-20">
+            {allNews?.map((news, i) => {
+              return <News key={i} newsHeadline={news[0]} time={news[2]} />;
+            })}
+          </div>
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,9 @@ import MainPage from "../components/MainPage/MainPage";
 
 export default function Home() {
   return (
-    <div className="w-full h-full">
-      <div className="w-full bg-bgBlackSec h-auto">
-        <p className="text-center text-2xl  font-bold uppercase underline decoration-from-font decoration-solid decoration-green-700 underline-offset-4 ">
-          STOKS
-        </p>
+    <div className="w-full h-full ">
+      <div className="w-full py-2 border-b-[1px] border-bgWhiteSec/30 bg-bgBlackSec h-auto">
+        <p className="text-center text-2xl  font-bold uppercase ">STOKS</p>
       </div>
       <MainPage />
     </div>
